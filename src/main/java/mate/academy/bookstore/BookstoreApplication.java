6a4fd1241208bfb8d39f11b3e796033d60f4cb75
Влад <dakadofordisluv@gmail.com>
2023-09-05 04:05:26 +0300

    remove redundant imports

--- src/main/java/mate/academy/bookstore/BookstoreApplication.java
+++ src/main/java/mate/academy/bookstore/BookstoreApplication.java
@@ -1,13 +1,9 @@
 package mate.academy.bookstore;
 
-import java.math.BigDecimal;
 import lombok.RequiredArgsConstructor;
-import mate.academy.bookstore.dto.CreateBookRequestDto;
 import mate.academy.bookstore.service.BookService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @RequiredArgsConstructor
 @SpringBootApplication
