cb6202315ea19ad129ffb83511b2c21db20411cc
Влад <dakadofordisluv@gmail.com>
2023-09-21 00:15:48 +0300

    added swagger changes

--- src/main/java/mate/academy/bookstore/config/SecurityConfig.java
+++ src/main/java/mate/academy/bookstore/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
-                                .requestMatchers("/auth/**")
+                                .requestMatchers("/auth/**", "/swagger-ui/**",
+                                        "/v3/api-docs/**", "/error")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
