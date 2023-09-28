7f1fe81254b5d98b5cb55dc2624011b4f6bb4feb
Влад <dakadofordisluv@gmail.com>
2023-09-28 14:16:08 +0300

    added @Accessors to models

--- src/main/java/mate/academy/bookstore/model/Category.java
+++ src/main/java/mate/academy/bookstore/model/Category.java
@@ -7,11 +7,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.experimental.Accessors;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 @Entity
 @Table(name = "categories")
+@Accessors(chain = true)
 @SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id=?")
 @Where(clause = "is_deleted = false")
 @Data
