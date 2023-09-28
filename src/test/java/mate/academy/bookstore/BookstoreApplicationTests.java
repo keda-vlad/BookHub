e7fe595e6d104cf651057561d5149aa88cc6eaee
Влад <dakadofordisluv@gmail.com>
2023-09-28 14:19:02 +0300

    added config

--- src/test/java/mate/academy/bookstore/BookstoreApplicationTests.java
+++ src/test/java/mate/academy/bookstore/BookstoreApplicationTests.java
@@ -1,8 +1,11 @@
 package mate.academy.bookstore;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookstoreApplicationTests {
-
+    @Test
+    public void contextLoads(){
+    }
 }
